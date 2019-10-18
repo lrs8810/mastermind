858ed04e4dcd1e7999ebb0b2f2e5d6baefe7e60f
Laura Schulz <lrs8810@gmail.com>
2019-10-18 11:01:07 -0600

    Update runner file and add code and code test file

--- lib/runner.rb
+++ lib/runner.rb
@@ -15,22 +15,30 @@ class Runner
     system 'clear'
 
     loop do
-      if input == 'p'
+      if input == 'q'
+        print "See you next time!\n"
+        break
+      elsif input == 'i'
+        print "Your objective is to guess the sequence of the four elements in as few of tries as possible. Do you want to play?\n"
+        input = nil
+        input = gets.chomp.downcase
+      elsif input == 'p'
         print "I have generated a beginner sequence with four elements made up of : r(ed), (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.\nWhat's your guess?\n"
         guess = gets.chomp.downcase
         validate_guess(guess)
         break
-      elsif input == 'i'
-        print "Hello"
-      elsif input == 'q'
-        print "See you next time\n"
       else
-        "Invalid, please try again: "
+        print "Invalid, please try again. Enter (p)lay, read the (i)structions, or (q)uit.\n"
+        system 'clear'
+        input = nil
+        input = gets.chomp.downcase
       end
     end
   end
 
-  def validate_guess(guess)
-
+  def validate_guess(guess, code)
+    if guess == code
+      print "Congratulations! You guessed the sequence #{code} in x guesses over x minutes, x seconds."
+    end
   end
 end
